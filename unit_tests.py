bdcc13009955cfce10116e0f9d37559b954aed12
Matthew W. Samsonoff <matthew.samsonoff@makerbot.com>
2012-05-22 09:32:49 -0400

    Python 2.6/2.7 portability fix.

--- unit_tests.py
+++ unit_tests.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
- 
-import unittest2 as unittest
- 
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
 if __name__ == "__main__":
     all_tests = unittest.TestLoader().discover('tests', pattern='*.py')
     unittest.TextTestRunner().run(all_tests)
