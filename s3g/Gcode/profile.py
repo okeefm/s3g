ba9687be38c7f84e9f4aadfccb03173b44878ce3
Dave Sayles <sayles.dave@gmail.com>
2012-06-11 17:45:12 -0400

    added documentation for gcode/profile.py

--- s3g/Gcode/profile.py
+++ s3g/Gcode/profile.py
@@ -1,5 +1,5 @@
 """
-A machine profile object that holds all values for a specific profile
+A machine profile object that holds all values for a specific profile.
 """
 
 import json
@@ -7,6 +7,9 @@ import os
 class Profile(object):
 
   def __init__(self, name):
+    """Constructor for the profile object.
+    @param string name: Name of the profile, NOT the path.
+    """
     self.path = os.path.join(
         os.path.abspath(os.path.dirname(__file__)),'./profiles/')  #Path of the profiles directory
     extension = '.json'
