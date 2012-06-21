5edf9daea3e87f5bd83b8f82515501ae9decd52a
Dave Sayles <sayles.dave@gmail.com>
2012-06-21 15:12:16 -0400

    added supressor for warning messages during unit_test.py

--- unit_tests.py
+++ unit_tests.py
@@ -4,7 +4,10 @@ try:
     import unittest2 as unittest
 except ImportError:
     import unittest
+import warnings
 
 if __name__ == "__main__":
+  with warnings.catch_warnings():
+    warnings.simplefilter("ignore")  
     all_tests = unittest.TestLoader().discover('tests', pattern='*.py')
     unittest.TextTestRunner().run(all_tests)
