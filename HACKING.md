4830823d407ac1e60bae01528b9302146614d8f1
Far McKon <FarMcKon@gmail.com>
2012-07-11 11:29:36 -0400

    commit to test git decribe

---
+++ HACKING.md
@@ -0,0 +1,15 @@
+
+
+Creating an egg
+---
+
+Updating the version
+--- 
+run git tag to a new version $git tag -a v0.5.0.5-beta -m 'v0.5.0.5-beta'
+
+
+Update MANIFEST.in
+Update ???
+Update ????
+
+
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='pys3g',
-    version='0.1.0',
+    version='0.5.0-Beta',
     author='MakerBot Industries',
     author_email='matt.mets@makerbot.com',
     packages=['s3g', 's3g.test'],
     url='http://github.com/makerbot/s3g',
-    license='LICENSE.txt',
-    description='.',
+    license='COPYING.txt',
+    description='3d printer drivers in python, by MakerBot',
     long_description=open('README.md').read(),
 )
 
