cde4e39ff1f2ddf1df2ba65f93406e7bbaaadc2b
Far McKon <FarMcKon@gmail.com>
2012-07-11 12:02:41 -0400

    updated to work with setup.Setup now fetches version from s3g/__init__.py version variable

--- s3g/__init__.py
+++ s3g/__init__.py
@@ -1,5 +1,7 @@
 __all__ = ['Encoder', 'FileReader', 'Gcode', 'Writer', 's3g', 'profile', 'constants', 'errors']
 
+__version__ = 'v0.5.0.0'
+
 import Encoder
 import FileReader
 import Gcode
--- setup.py
+++ setup.py
@@ -1,14 +1,44 @@
+#Setup.py based on pyserial/pyserial/setup.py
+
+import sys
+
 from distutils.core import setup
 
+
+if sys.version_info < (2,6):
+	raise ValueError("Sorry, python versions older than 2.6"	
+					" are not supported")
+elif sys.version_info >= (3, 0):
+    try:
+        from distutils.command.build_py import build_py_2to3 as build_py
+        from distutils.command.build_scripts import build_scripts_2to3 as build_scripts
+    except ImportError:
+        raise ImportError("build_py_2to3 not found in distutils - it is required for Python 3.x")
+    suffix = "-py3k"
+else:
+    from distutils.command.build_py import build_py
+    from distutils.command.build_scripts import build_scripts
+    suffix = ""
+
+import re, os
+version = re.search(
+        "__version__.*'(.+)'",
+        open(os.path.join('s3g', '__init__.py')).read()).group(1)
+
+
 setup(
-    name='pys3g',
-    version='0.5.0-Beta',
+    name='pys3g' + suffix, 
+    version=version,
     author='MakerBot Industries',
     author_email='matt.mets@makerbot.com',
-    packages=['s3g', 's3g.test'],
+    packages=['s3g', 's3g.tests'],
     url='http://github.com/makerbot/s3g',
     license='COPYING.txt',
     description='3d printer drivers in python, by MakerBot',
     long_description=open('README.md').read(),
+	#classifiers = []
+	platforms = 'any',
+	#cmdclass = { ? : ? }
+	#scripts = ['/scripts/file']
 )
 
