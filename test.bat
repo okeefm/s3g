02ec74b1a2a944cbca086320fc94009e051f3d81
Filipp Gelman <filipp@makerbot.com>
2012-07-23 14:07:16 -0400

    Proper passing of the exit code

--- test.bat
+++ test.bat
@@ -8,9 +8,8 @@ IF NOT EXIST obj MD obj
 
 CALL setup.bat
 python unit_tests.py
-_CODE = $?
 
 ENDLOCAL EnableDelayedExpansion
 CALL stop.bat
-REM Don't do this in windows, it closes your command prompt
-REM exit _CODE
+REM exit /b code will properly return error codes
+exit /b %ERRORLEVEL%
