c90e83cd4bdea1df0d4134a8c84f212038979b32
Dave Sayles <sayles.dave@gmail.com>
2012-07-24 10:20:06 -0400

    Added some documentation.

--- s3g/machine_detector.py
+++ s3g/machine_detector.py
@@ -1,3 +1,13 @@
+"""
+A Machine Detector that can detect machines we would like
+to connect to.  Can detect multiple machines by passing
+in a list of machine names.  We search through their machine
+profiles for their VID/PID information, use makerbot's pyserial
+library to find any/all ports with those values, and compare iSerial
+values to find all current, added and removed ports.  All ports
+are kept track of in a python dict named "ports".
+"""
+
 import serial.tools.list_ports
 import profile
 
@@ -48,11 +58,25 @@ class MachineDetector(object):
     return current_ports, added_ports, removed_ports
 
   def begin_scanning(self, machines):
+    """
+    Given a list of machines, begins scanning for those machines.
+    First it sets up the python dict used to keep track of all
+    those machines.
+
+    @param list machines: List of machines being scanned for
+    """
     self.reset_port_list(machines)
     while True:
       self.scan_multiple_ports(machines)
 
   def reset_port_list(self, machines):
+    """
+    Given a list of machines, sets up a python dict to 
+    keep track of current, added and removed machines
+
+    @param list machiens: List of machines we are going to be
+        scanning for
+    """
     for machine in machines:
       self.ports[machine] = {
           'current_ports' : [],
@@ -61,22 +85,18 @@ class MachineDetector(object):
         } 
 
   def scan_multiple_ports(self, machines):
+    """
+    Given a list of machines, iterates through each one
+    and gets their VId/PID values, and searches for them.
+    If any are found, puts the appropriate values in the 
+    python dict used to keep track of them.
+
+    @param machines: List of machines we are going to be 
+        scanning for
+    """
     for machine in machines:
       vid, pid = self.get_vid_pid(machine)
       new_ports = self.scan_serial_ports(self.ports[machine]['current_ports'], vid, pid)
       self.ports[machine]['current_ports'] = new_ports[0]
       self.ports[machine]['added_ports'] = new_ports[1]
       self.ports[machine]['removed_ports'] = new_ports[2]
-      
-if __name__ == "__main__":
-  current_ports = {}
-  while True:
-    current_ports, added_ports, removed_ports = scan_serial_ports(current_ports)
- 
-    for port in added_ports:
-      print "port added: ", port, added_ports[port]
- 
-    for port in removed_ports:
-      print "port removed:", port, removed_ports[port]
-
-    time.sleep(1)
