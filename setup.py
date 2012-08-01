b375901e1ee2996db791d4821e56986ff9554f02
Adam Mayer <phooky@gmail.com>
2012-08-01 17:56:39 -0400

    We have a successful package; now we need to install the profiles

--- setup.py
+++ setup.py
@@ -10,7 +10,15 @@ setup(
     version=version,
     author='MakerBot Industries',
     author_email='matt.mets@makerbot.com',
-    packages=['s3g'],
+    packages=[
+        's3g',
+        's3g.Encoder',
+        's3g.FileReader',
+        's3g.Firmware',
+        's3g.Gcode',
+        's3g.Preprocessors',
+        's3g.Writer'
+    ],
     url='http://github.com/makerbot/s3g',
     license='LICENSE.txt',
     description='.',
