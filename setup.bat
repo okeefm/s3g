a9a05003d67a72396317fdb2f35663ddffe7a119
Filipp Gelman <filipp@makerbot.com>
2012-08-02 16:02:22 -0400

    Windows setup.bat sets pythonpath environment variable

--- setup.bat
+++ setup.bat
@@ -5,6 +5,9 @@ IF NOT EXIST virtualenv GOTO DIRNOTEXISTS
 CALL virtualenv\Scripts\activate
 pip install -q --use-mirrors argparse mock coverage doxypy lockfile unittest-xml-reporting
 easy_install submodule/conveyor_bins/pyserial-2.7_mb-py2.7.egg
+
+SET PYTHONPATH=%PYTHONPATH%;%CD%
+
 GOTO DONE
 
 :DIRNOTEXISTS
@@ -12,3 +15,6 @@ python virtualenv.py --system-site-packages virtualenv
 GOTO DIREXISTS
 
 :DONE
+
+
+
