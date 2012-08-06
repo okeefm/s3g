0c07d2be3a03a423dadfa6578837d83d1269ff55
Dave Sayles <sayles.dave@gmail.com>
2012-08-06 11:11:21 -0400

    added EepromMap.hh parser, parsed eeprom maps

--- s3g/EEPROM/eeprom_analyzer.py
+++ s3g/EEPROM/eeprom_analyzer.py
@@ -15,6 +15,14 @@ dict in the form of:
 }
 """
 
+class EndOfNamespaceError(IOError):
+  def __init__(self):
+    pass
+
+class EndOfEepromError(IOError):
+  def __init__(self):
+    pass
+
 import json
 
 class eeprom_analyzer(object):
@@ -22,6 +30,77 @@ class eeprom_analyzer(object):
   def __init__(self):
     self.filename = 'eeprom_map.json'
     self.eeprom_map = {}
+    self.necessary_eeproms = {
+        'toolhead_eeprom_offsets' : False,
+        'eeprom_offsets' : False,
+        }
+
+  def parse_file(self):
+    try:
+      while True:
+        namespace_name = self.find_next_namespace().lower()
+        if namespace_name in self.necessary_eeproms:
+          self.necessary_eeproms[namespace_name] = True
+        namespace = {}
+        try:
+          while True:
+            self.find_next_entry()
+            pack_type = self.parse_out_type(self.file.readline())
+            (name, location) = self.parse_out_name_and_location(self.file.readline())
+            namespace[name] = {
+                'location'  : location,
+                'type'      : pack_type,
+                }
+        except EndOfNamespaceError:
+          eeprom_map = {
+              namespace_name  : namespace,
+              }
+          self.dump_json(namespace_name, eeprom_map)
+    except EndOfEepromError:
+      pass
+      
+  def find_next_entry(self):
+    namespace_end = '}'
+    entry_line = '//$BEGIN_ENTRY'
+    line = self.file.readline()
+    while entry_line not in line:
+      if namespace_end in line:
+        raise EndOfNamespaceError
+      line = self.file.readline()
+    return line
+
+  def find_next_namespace(self):
+    """
+    Reads lines from the file until a line that 
+    starts with namespace is encountered.  The name 
+    of that namespace is then parsed out and returned.
+
+    @return str name: The name of the current namespace
+    """
+    namespace = 'namespace'
+    end_of_eeprom = '#endif'
+    line = self.file.readline()
+    while not line.startswith(namespace):
+      if end_of_eeprom in line:
+        raise EndOfEepromError
+      line = self.file.readline()
+    return self.parse_out_namespace_name(line)
+
+  def parse_out_namespace_name(self, line):
+    """
+    Given a line in the form of 
+      "namespace <name> {"
+    parses out the name
+  
+    @param str line: The line with the namespace
+    @return str name: The name of the namespace 
+    """
+    line = line.strip()
+    for s in ['\n', '\r', '\t', '{']:
+      line = line.rstrip(s)
+    line = line.lstrip('namespace')
+    line = line.replace(' ', '')
+    return line
 
   def parse_out_name_and_location(self, line):
     """
@@ -37,11 +116,23 @@ class eeprom_analyzer(object):
       line = line.replace(w, '')
     for s in ["\r", "\n", ";"]:
       line = line.rstrip(s)
+    line = line.replace('\t', '')
     line = line.replace(" ", "")
     (name, location) = line.split("=")
     return name, location
 
-  def dump_json(self):
-    output = json.dumps(self.eeprom_map)
-    with(open(self.filename, 'w')) as f:
+  def parse_out_type(self, line):
+    line = line.strip()
+    for s in ['\n', '\r','\t', ]:
+      line = line.rstrip(s)
+    line = line.lstrip('//')
+    parts = line.split(':')
+    if parts[0] == '$S':
+      return parts[1]
+    else:
+      return line
+
+  def dump_json(self, name, eeprom_map):
+    output = json.dumps(eeprom_map)
+    with(open(name, 'w')) as f:
       f.write(output)
--- tests/test_eeprom_analyzer.py
+++ tests/test_eeprom_analyzer.py
@@ -25,6 +25,41 @@ class TestLineReader(unittest.TestCase):
       got_return = self.reader.parse_out_name_and_location(line+s)
       self.assertEqual(expected_return, got_return)
 
+  def test_parse_out_namespace_name(self):
+    lines = [
+        'namespace toolhead_eeprom {\n',
+        'namespace toolhead_eeprom {\r',
+        'namespace toolhead_eeprom {',
+        '   namespace toolhead_eeprom {',
+        'namespace  toolhead_eeprom {         ',
+        'namespace      toolhead_eeprom {',
+        'namespace toolhead_eeprom     {',
+        ]
+    expected_name = 'toolhead_eeprom'
+    for line in lines:
+      got_name = self.reader.parse_out_namespace_name(line)
+      self.assertEqual(expected_name, got_name)
+
+  def test_parse_out_type_different_endings(self):
+    lines = [
+      '//$S:iiii\n',
+      '//$S:iiii\r',
+      '//$S:iiii    \r',
+      ]
+    expected = 'iiii'
+    for line in lines:
+      self.assertEqual(expected, self.reader.parse_out_type(line))
+
+  def test_parse_out_type_different_types(self):
+    lines = {
+        'iiii'  : '//$S:iiii\n',
+        'HH'    : '//$S:HH\r',
+        's'     : '//$S:s\n',
+        'Hhiis' : '//$S:Hhiis',
+        }
+    for line in lines:
+      self.assertEqual(line, self.reader.parse_out_type(lines[line]))
+    
   def testDumpJSON(self):
     test_dic = {
         'a' : 1,
@@ -35,9 +70,9 @@ class TestLineReader(unittest.TestCase):
       pass
     input_path = input_file.name
     os.unlink(input_path)
-    self.reader.filename = input_path
-    self.reader.eeprom_map = test_dic
-    self.reader.dump_json()
+    filename = input_path
+    eeprom_map = test_dic
+    self.reader.dump_json(filename, eeprom_map)
     with open(input_path) as f:
       written_vals = json.load(f)
     self.assertEqual(test_dic, written_vals)
