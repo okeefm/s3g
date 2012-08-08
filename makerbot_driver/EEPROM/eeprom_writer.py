56273bebfd93631ff4c80027b3a2b641eee136fa
Dave Sayles <sayles.dave@gmail.com>
2012-08-08 18:55:27 -0400

    added documentation.

--- makerbot_driver/EEPROM/eeprom_writer.py
+++ makerbot_driver/EEPROM/eeprom_writer.py
@@ -45,6 +45,13 @@ class EepromWriter(object):
         self.s3g.write_to_EEPROM(data[0], data[1])
 
   def search_for_toolhead_entry_and_offset(self, name, the_map, toolhead):
+    """Assuming we are looking for a toolhead eeprom value, find the 
+    appropriate toolhead sub_map name and pull the correct values.
+
+    @param str name: The name we are looking for
+    @param dict the_map: The map we are looking in
+    @param int toolhead: The toolhead we are looking for
+    """
     toolhead_dict_name = self.get_toolhead_dict_name(toolhead)
     found_dict = the_map[toolhead_dict_name]['sub_map'][name]
     offset = int(the_map[toolhead_dict_name]['offset'], 16)
@@ -84,51 +91,6 @@ class EepromWriter(object):
     eeprom offsets
     """
     return 'T%i_DATA_BASE' %(toolhead)
-
-  def get_offset(self, input_dict, sub_map_name='eeprom_offsets', toolhead=None):
-    """Given an input dict, sub_map_name and toolhead, gets the offset for a 
-    particular value.  An eeprom entry is stored on either the main eeprom_offsets
-    map, or a submap.  Additionally, since there are multiple toolhead submaps, 
-    we need to specify the toolhead offset we want to write to.  If a toolhead
-    is specified w/ a non-toolhead sub_map_name, we throw an error.  If a sub_map
-    is specified but not found, we throw an error.
-
-    @param dict input_dict: The dictionary containing all the information needed
-      for an eeprom entry
-    @param str sub_map_name: The name of the possible sub_map that contains the
-      entry.
-    @param int toolhead: The toolhead (if any) we want to write to
-    @return int offset: The calculated offset for this eeprom entry
-    """
-    #If were lookin at a toolhead eeprom value, but the submap isnt toolhead_eeprom,
-    #something went wrong
-    if toolhead is not None and sub_map_name is not 'toolhead_offsets':
-      raise ToolheadSubMapError(sub_map_name)
-    #Set initial offset
-    offset = int(input_dict['offset'], 16)
-    #If were lookin at a toolhead eeprom value
-    if toolhead is not None:
-      #We can assume the name, so just pull the value
-      offset += int(self.eeprom_map[self.main_map][self.get_toolhead_dict_name(toolhead)]['offset'], 16)
-    #else we have to look at each entry for the offset
-    elif sub_map_name is not self.main_map:
-      sub_map_offset = -1
-      #Save a copy of the main eeprom map we want to explore
-      the_map = self.eeprom_map[self.main_map]
-      for key in the_map:
-        try:
-          #If this filed exists, it will resolve successfully
-          if the_map[key]['eeprom_map'] == sub_map_name: 
-            sub_map_offset = int(the_map[key]['offset'], 16)
-        #Otherwise we except the KeyError and pass
-        except KeyError:
-          pass
-      #If we were looking for the sub_map, and didnt find it, throw
-      if sub_map_offset is -1:
-        raise SubMapNotFoundError(sub_map_name)
-      #Add the offsets together
-      offset += sub_map_offset
-    return offset
 
   def good_string_type(self, t):
     """
