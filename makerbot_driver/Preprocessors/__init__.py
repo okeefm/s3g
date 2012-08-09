a39c0158c9125b50490c9d17be133b5697fa3157
Dave Sayles <sayles.dave@gmail.com>
2012-08-09 17:02:13 -0400

    added singletonTPrepro., tests

--- makerbot_driver/Preprocessors/__init__.py
+++ makerbot_driver/Preprocessors/__init__.py
@@ -1,6 +1,7 @@
-all = ['Preprocessor', 'Skeinforge50Preprocessor', 'ToolchangePreprocessor', 'errors']
+all = ['Preprocessor', 'Skeinforge50Preprocessor', 'ToolchangePreprocessor', 'SingletonTPreprocessor', 'errors']
 
 from preprocessor import *
 from skeinforgePreprocessors import *
 from toolchangePreprocessor import *
+from singletonTPreprocessor import *
 from errors import *
