07ba9c7562db790d22176e8474384a9a19c62e90
Dave Sayles <sayles.dave@gmail.com>
2012-08-10 14:10:24 -0400

    removed deprecated error

--- makerbot_driver/EEPROM/errors.py
+++ makerbot_driver/EEPROM/errors.py
@@ -1,11 +1,3 @@
-class MissingVariableError(Exception):
-  """MissingVariableErrors are thrown when the eeprom
-  reader tries to read a value off the eeprom, but it
-  cannot due to a missing variable (i.e. offset, type, etc)
-  """
-  def __init__(self, value):
-    self.value = value
-
 class NonTerminatedStringError(Exception):
   """NonTerminatedStringErrors are raised when a string is parsed that does not have a null-terminator on it
   """
