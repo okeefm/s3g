c7aa4f0f2c9c5135b7e3c02af5fd050c8d8b7225
Dave Sayles <sayles.dave@gmail.com>
2012-08-14 11:27:58 -0400

    added coordinate system parser (remove g54/55)

---
+++ tests/test_CoordinatePreprocessor.py
@@ -0,0 +1,33 @@
+import os
+import sys
+lib_path = os.path.abspath('../')
+sys.path.append(lib_path)
+
+import unittest
+import tempfile
+
+import makerbot_driver
+
+class CoordinatePreprocessor(unittest.TestCase):
+  
+  def setUp(self):
+    self.cp = makerbot_driver.Preprocessors.CoordinatePreprocessor()
+    
+  def tearDown(self):
+    self.cp = None
+
+  def test_transform_g54(self):
+    cases = [
+        ['G54\n'  , '',],
+        ['G55\n'  , 'G55\n',],
+        ]
+    for case in cases:
+      self.assertEqual(case[1], self.cp._transform_g54(case[0]))
+
+  def test_transform_g55(self):
+    cases = [
+        ['G55\n', '',],
+        ['G54\n', 'G54\n'],
+        ]
+    for case in cases:
+      self.assertEqual(Case[1], self.cp_transform_g55(case[0]))
