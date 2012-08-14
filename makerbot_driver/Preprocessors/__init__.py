cc367e4901c3dc367ad6975fadec9a4cdc1d8582
Dave Sayles <sayles.dave@gmail.com>
2012-08-14 09:35:06 -0400

    fixed potential git problem (Since renaming didnt seem to work.....)

--- makerbot_driver/Preprocessors/__init__.py
+++ makerbot_driver/Preprocessors/__init__.py
@@ -1,7 +1,7 @@
 all = ['Preprocessor', 'Skeinforge50Preprocessor', 'ToolchangePreprocessor', 'SingletonTPreprocessor', 'errors']
 
-from Preprocessor import *
-from SkeinforgePreprocessors import *
-from ToolchangePreprocessor import *
-from SingletonTPreprocessor import *
+from preprocessor import *
+from skeinforgePreprocessors import *
+from toolchangePreprocessor import *
+from singletonTPreprocessor import *
 from errors import *
