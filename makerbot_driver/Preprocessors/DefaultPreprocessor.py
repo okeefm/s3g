fc66e903b2ae4d2f9579491e316000d026fde0c6
Dave Sayles <sayles.dave@gmail.com>
2012-08-16 13:39:49 -0400

    updated default preprocessor

--- makerbot_driver/Preprocessors/DefaultPreprocessor.py
+++ makerbot_driver/Preprocessors/DefaultPreprocessor.py
@@ -4,7 +4,9 @@ that runs several sub-preprocessors to cover
 common problems that most gcode files have.
 """
 
-from toolchangePreprocessor import *
+import tempfile
+
+import makerbot_driver
 
 class DefaultPreprocessor(object):
 
@@ -12,5 +14,9 @@ class DefaultPreprocessor(object):
     pass
 
   def process_file(self, input_path, output_path):
-    toolPrepro = ToolchangePreprocessor()
-    toolPrepro.process_file(input_path, output_path)
+    with tempfile.NamedTemporaryFile(suffix='.gcode', delete=True) as f:
+      toolchange_path = f.name
+    tool_prepro = makerbot_driver.Preprocessors.ToolchangePreprocessor()
+    tool_prepro.process_file(input_path, toolchange_path)
+    coordinate_prepro = makerbot_driver.Preprocessors.CoordinatePreprocessor()
+    coordinate_prepro.process_file(toolchange_path, output_path_)
