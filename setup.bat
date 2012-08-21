09019a56e5bbae647c8be318aac7e4c615612f3a
Adam Mayer <phooky@gmail.com>
2012-08-21 12:33:56 -0400

    Updating submodule and reducing to a single egg

--- setup.bat
+++ setup.bat
@@ -4,7 +4,7 @@ IF NOT EXIST virtualenv GOTO DIRNOTEXISTS
 :DIREXISTS
 CALL virtualenv\Scripts\activate
 pip install -q --use-mirrors argparse mock coverage doxypy lockfile unittest-xml-reporting
-easy_install submodule/conveyor_bins/pyserial-2.7_mb-py2.7.egg
+easy_install submodule/conveyor_bins/pyserial-2.7_mb2.1-py2.7.egg
 
 SET PYTHONPATH=%PYTHONPATH%;%CD%
 
