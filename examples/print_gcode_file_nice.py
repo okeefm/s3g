d9efa290123c2305e504b00b8020bf57e2dd6252
Filipp Gelman <filipp@makerbot.com>
2012-08-27 16:45:08 -0400

    Fix for -s option

--- examples/print_gcode_file_nice.py
+++ examples/print_gcode_file_nice.py
@@ -18,33 +18,33 @@ argparser.error=argparse_error_override
 #MONKEY PATCH END
 argparser.add_argument(
     '-t', '--toolheads',
-    dest='toolheads', 
-    help='which toolheads to heat', 
-    choices=['left', 'right', 'both'], 
+    dest='toolheads',
+    help='which toolheads to heat',
+    choices=['left', 'right', 'both'],
     default='both')
 argparser.add_argument(
     '--platform',
-    dest='platform', 
-    help='heat platform (default off)', 
+    dest='platform',
+    help='heat platform (default off)',
     action='store_true')
 argparser.add_argument(
     '-m', '--machine',
-    dest='machine', 
-    help='Quoted name of bot (\'The Replicator\')', 
+    dest='machine',
+    help='Quoted name of bot (\'The Replicator\')',
     default='The Replicator')
 argparser.add_argument(
     '-p', '--port',
-    dest='port', 
-    help='serial port for bot', 
+    dest='port',
+    help='serial port for bot',
     default=None)
 argparser.add_argument(
     '-s', '--startend',
-    dest='startend', 
-    help='Generate start and end gcode (if your gcode does not already have one)', 
+    dest='startend',
+    help='Indicates your gcode has start and end gcode already',
     action='store_true')
 argparser.add_argument(
     metavar='filename',
-    dest='filename', 
+    dest='filename',
     help='name of gcode file')
 parsed=argparser.parse_args()
 
@@ -63,11 +63,11 @@ if port is None:
 if port is None:
     print "Cant Find %s" %(machine)
     sys.exit()
-try:
+else:
     print("Port: " + str(port))
     factory = makerbot_driver.BotFactory()
     r, prof = factory.build_from_port(port)
-    
+
     print((tool0,tool1,material,machine))
 
     assembler = makerbot_driver.GcodeAssembler(prof)
@@ -84,20 +84,17 @@ try:
     parser.state.values["build_name"] = filename[:15]
     parser.state.profile = prof
     parser.s3g = r
-    
-    if parsed.startend:
+
+    if not parsed.startend:
         for line in start_gcode:
             parser.execute_line(line)
-    with open(parsed.FILENAME, 'r') as f:
+    with open(parsed.filename, 'r') as f:
         for line in f:
+            print(line)
             parser.execute_line(line)
-    if parsed.startend:
+    if not parsed.startend:
         for line in end_gcode:
             parser.execute_line(line)
     exit(0)
-    
-except Exception as e:
-    print('This exception happened')
-    print(e)
-    exit(1)
+
     
