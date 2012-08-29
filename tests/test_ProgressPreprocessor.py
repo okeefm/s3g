eedae1d857a2e46d3f958a42485b3fd9456b77a4
Filipp Gelman <filipp@makerbot.com>
2012-08-29 11:58:51 -0400

    More comprehensive progress preprocessor unit test

--- tests/test_ProgressPreprocessor.py
+++ tests/test_ProgressPreprocessor.py
@@ -1,11 +1,14 @@
 import os
 import sys
+import re
 lib_path = os.path.abspath('../')
 sys.path.append(lib_path)
 
 import unittest
 import tempfile
 import makerbot_driver 
+
+progress_command = re.compile('^\s*M73 P(\d+)')
 
 class TestProgressPreprocessor(unittest.TestCase):
 
@@ -14,6 +17,39 @@ class TestProgressPreprocessor(unittest.TestCase):
 
   def tearDown(self):
     self.p = None
+  
+  def extract_progress_commands(self, listoflines):
+    """
+    Accepts list of gcode lines
+    Returns list of only progress commands
+    progress command is a tuple of the command string and percent int
+    """
+    progresslist = []
+    for line in listoflines:
+      matchresult = progress_command.match(line)
+      if matchresult:
+        progresslist.append((line, int(matchresult.group(1))))
+    return progresslist
+  
+  def test_sanity_check(self):
+    sample = """
+    M73 P0
+    M73 P1
+    M73 P5
+    M73 P50
+    M73 P100
+    """
+    progress = self.extract_progress_commands(sample.splitlines())
+    percents = [p[1] for p in progress]
+    expected = [0, 1, 5, 50, 100]
+    self.assertEqual(expected, percents)
+  
+  def isincreasing(self, linesofgcode):
+    #extract progress from lines
+    progress = self.extract_progress_commands(linesofgcode)
+    percents = [p[1] for p in progress]
+    for i in range(1, len(percents)):
+      self.assertTrue(percents[i-1] < percents[i])
 
   def test_process_file(self):
     the_input = "G1 X50 Y50\nG1 X0 Y0 A50\nG1 X0 Y0 B50\nG1 X0 Y0 B50\nG1 X0 Y0 B50\nG1 X0 Y0 A50\nG1 X0 Y0 B50\n"
@@ -24,7 +60,9 @@ class TestProgressPreprocessor(unittest.TestCase):
       output_path = output.name
     self.p.process_file(input_file.name, output_path)
     with open(output_path) as exp:
-      self.assertEqual(expected_output, exp.read())
+      outdata = exp.read()
+      self.assertEqual(expected_output, outdata)
+      self.isincreasing(outdata.splitlines())
 
 if __name__ == '__main__':
   unittest.main()
