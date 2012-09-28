0c97def995910d5d24364aaac2e29a6aad5bbefd
Dave Sayles <sayles.dave@gmail.com>
2012-09-28 17:48:56 -0400

    * Toolchange Processor optimization.

--- makerbot_driver/GcodeProcessors/SingletonTProcessor.py
+++ makerbot_driver/GcodeProcessors/SingletonTProcessor.py
@@ -13,10 +13,9 @@ class SingletonTProcessor(LineTransformProcessor):
         self.code_map = {
             re.compile("[^(;]*([(][^)]*[)][^(;]*)*[tT][0-9]"): self._transform_singleton
         }
-        self.singleton_search = re.compile("[^(;]*([(][^)]*[)][^(;]*)*[tT][0-9]")
+        self.singleton_search = re.compile("[^(;]*([(][^)]*[)][^(;]*)*[tT]([0-9])")
 
     def _transform_singleton(self, input_line):
         m = re.match(self.singleton_search, input_line)
-        the_match = m.group()
-        return_line = 'M135 T%s\n' % (the_match[-1])
+        return_line = 'M135 T%s\n' % (m.group(2))
         return return_line
