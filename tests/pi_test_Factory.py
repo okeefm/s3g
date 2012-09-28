5977a44c61012fae798bbfe954f1ed6f9b4eb525
Dave Sayles <sayles.dave@gmail.com>
2012-09-28 15:37:44 -0400

    * Pep8 some files

--- tests/pi_test_Factory.py
+++ tests/pi_test_Factory.py
@@ -9,6 +9,7 @@ import tempfile
 
 import makerbot_driver
 
+
 class TestFactory(unittest.TestCase):
 
     def setUp(self):
@@ -22,75 +23,87 @@ class TestFactory(unittest.TestCase):
     def test_create_parser_legacy(self):
         machine_name = 'TOMStepstruder'
         parser = makerbot_driver.create_parser(machine_name, legacy=True)
-        self.assertTrue(parser.__class__.__name__=='GcodeParser')
+        self.assertTrue(parser.__class__.__name__ == 'GcodeParser')
         self.assertEqual(getattr(parser, 's3g'), None)
-        self.assertTrue(parser.state.__class__.__name__=='LegacyGcodeStates')
-        self.assertTrue(parser.state.profile.values['type']=="Thing-O-Matic with Stepstruder Mk7")
-        
+        self.assertTrue(parser.state.__class__.__name__ == 'LegacyGcodeStates')
+        self.assertTrue(parser.state.profile.values['type']
+                        == "Thing-O-Matic with Stepstruder Mk7")
+
     def test_create_parser(self):
         machine_name = 'ReplicatorSingle'
         parser = makerbot_driver.create_parser(machine_name, legacy=False)
-        self.assertTrue(parser.__class__.__name__=='GcodeParser')
+        self.assertTrue(parser.__class__.__name__ == 'GcodeParser')
         self.assertEqual(getattr(parser, 's3g'), None)
-        self.assertTrue(parser.state.__class__.__name__=='GcodeStates')
-        self.assertTrue(parser.state.profile.values['type']=="The Replicator Single")
+        self.assertTrue(parser.state.__class__.__name__ == 'GcodeStates')
+        self.assertTrue(
+            parser.state.profile.values['type'] == "The Replicator Single")
 
     def test_create_print_to_file_legacy(self):
         machine_name = 'TOMStepstruder'
         with tempfile.NamedTemporaryFile(suffix='.s3g', delete=True) as f:
             path = f.name
-        parser = makerbot_driver.create_print_to_file_parser(path, machine_name, legacy=True)
-        self.assertTrue(parser.__class__.__name__=='GcodeParser')
-        self.assertTrue(parser.s3g.__class__.__name__=='s3g')
-        self.assertTrue(parser.s3g.writer.__class__.__name__=='FileWriter')
-        self.assertTrue(parser.s3g.writer.file.name==path)
-        self.assertTrue(parser.state.__class__.__name__=='LegacyGcodeStates')
-        self.assertTrue(parser.state.profile.values['type']=='Thing-O-Matic with Stepstruder Mk7')
+        parser = makerbot_driver.create_print_to_file_parser(
+            path, machine_name, legacy=True)
+        self.assertTrue(parser.__class__.__name__ == 'GcodeParser')
+        self.assertTrue(parser.s3g.__class__.__name__ == 's3g')
+        self.assertTrue(parser.s3g.writer.__class__.__name__ == 'FileWriter')
+        self.assertTrue(parser.s3g.writer.file.name == path)
+        self.assertTrue(parser.state.__class__.__name__ == 'LegacyGcodeStates')
+        self.assertTrue(parser.state.profile.values['type']
+                        == 'Thing-O-Matic with Stepstruder Mk7')
 
     def test_create_print_to_file(self):
         machine_name = 'ReplicatorSingle'
         with tempfile.NamedTemporaryFile(suffix='.s3g', delete=True) as f:
             path = f.name
-        parser = makerbot_driver.create_print_to_file_parser(path, machine_name)
-        self.assertTrue(parser.__class__.__name__=='GcodeParser')
-        self.assertTrue(parser.s3g.__class__.__name__=='s3g')
-        self.assertTrue(parser.s3g.writer.__class__.__name__=='FileWriter')
-        self.assertTrue(parser.s3g.writer.file.name==path)
-        self.assertTrue(parser.state.__class__.__name__=='GcodeStates')
-        self.assertTrue(parser.state.profile.values['type']=='The Replicator Single')
+        parser = makerbot_driver.create_print_to_file_parser(
+            path, machine_name)
+        self.assertTrue(parser.__class__.__name__ == 'GcodeParser')
+        self.assertTrue(parser.s3g.__class__.__name__ == 's3g')
+        self.assertTrue(parser.s3g.writer.__class__.__name__ == 'FileWriter')
+        self.assertTrue(parser.s3g.writer.file.name == path)
+        self.assertTrue(parser.state.__class__.__name__ == 'GcodeStates')
+        self.assertTrue(
+            parser.state.profile.values['type'] == 'The Replicator Single')
 
     def test_create_print_to_stream_legacy(self):
         port = '/dev/tty.ACM0'
         machine_name = 'TOMStepstruder'
-        parser = makerbot_driver.create_print_to_stream_parser(port, machine_name, legacy=True)
-        self.assertTrue(parser.__class__.__name__=='GcodeParser')
-        self.assertTrue(parser.state.__class__.__name__=='LegacyGcodeStates')
-        self.assertTrue(parser.state.profile.values['type']=='Thing-O-Matic with Stepstruder Mk7')
+        parser = makerbot_driver.create_print_to_stream_parser(
+            port, machine_name, legacy=True)
+        self.assertTrue(parser.__class__.__name__ == 'GcodeParser')
+        self.assertTrue(parser.state.__class__.__name__ == 'LegacyGcodeStates')
+        self.assertTrue(parser.state.profile.values['type']
+                        == 'Thing-O-Matic with Stepstruder Mk7')
         self.from_filename_mock.assert_called_once_with(port)
-                
+
     def test_create_print_to_stream(self):
         port = '/dev/tty.ACM0'
         machine_name = 'ReplicatorSingle'
-        parser = makerbot_driver.create_print_to_stream_parser(port, machine_name)
-        self.assertTrue(parser.__class__.__name__=='GcodeParser')
-        self.assertTrue(parser.state.__class__.__name__=='GcodeStates')
-        self.assertTrue(parser.state.profile.values['type']=='The Replicator Single')
+        parser = makerbot_driver.create_print_to_stream_parser(
+            port, machine_name)
+        self.assertTrue(parser.__class__.__name__ == 'GcodeParser')
+        self.assertTrue(parser.state.__class__.__name__ == 'GcodeStates')
+        self.assertTrue(
+            parser.state.profile.values['type'] == 'The Replicator Single')
         self.from_filename_mock.assert_called_once_with(port)
 
     def test_create_eeprom_reader(self):
         port = '/dev/tty.ACM0'
         fw_version = 6.0
         working_directory = None
-        reader = makerbot_driver.create_eeprom_reader(port, fw_version, working_directory)
-        self.assertTrue(reader.__class__.__name__=='EepromReader')
+        reader = makerbot_driver.create_eeprom_reader(
+            port, fw_version, working_directory)
+        self.assertTrue(reader.__class__.__name__ == 'EepromReader')
         self.from_filename_mock.assert_called_once_with(port)
 
     def test_create_eeprom_reader(self):
         port = '/dev/tty.ACM0'
         fw_version = 6.0
         working_directory = None
-        writer = makerbot_driver.create_eeprom_writer(port, fw_version, working_directory)
-        self.assertTrue(writer.__class__.__name__=='EepromWriter')
+        writer = makerbot_driver.create_eeprom_writer(
+            port, fw_version, working_directory)
+        self.assertTrue(writer.__class__.__name__ == 'EepromWriter')
         self.from_filename_mock.assert_called_once_with(port)
 
 if __name__ == "__main__":
