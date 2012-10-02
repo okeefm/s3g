bbfa6c94ea1100299dfc3597763eb63497f7ef04
Dave Sayles <sayles.dave@gmail.com>
2012-10-02 11:25:13 -0400

    Added Skeinforge version checker

--- tests/pi_test_SkeinforgeProcessor.py
+++ tests/pi_test_SkeinforgeProcessor.py
@@ -23,11 +23,29 @@ class Skeinforge50ProcessorTests(unittest.TestCase):
         got_output = self.sp.process_gcode(gcodes)
         self.assertEqual(expected_output, got_output)
 
+    def test_process_file_bad_version(self):
+        gcodes = ["G21\n", "(*  using Skeinforge (49)  *)"]
+        self.assertRaises(makerbot_driver.GcodeProcessors.VersionError,
+                          self.sp.process_gcode, gcodes)
+
     def test_process_file_progress_updates(self):
-        gcodes = ["G90\n", "G21\n", "M104 S500\n", "M105 S500\n",
-                  "M101\n", "M102\n", "M108\n", "G1 X0 Y0 Z0 A0 B0\n"]
-        expected_output = ['G1 X0 Y0 Z0 A0 B0\n',
-                           'M73 P100 (progress (100%))\n']
+        gcodes = [
+            "(*  using Skeinforge (50)  *)\n",
+            "G90\n",
+            "G21\n",
+            "M104 S500\n",
+            "M105 S500\n",
+            "M101\n",
+            "M102\n",
+            "M108\n",
+            "G1 X0 Y0 Z0 A0 B0\n"
+        ]
+        expected_output = [
+            '(*  using Skeinforge (50)  *)\n',
+            'M73 P50 (progress (50%))\n',
+            'G1 X0 Y0 Z0 A0 B0\n',
+            'M73 P100 (progress (100%))\n',
+        ]
         got_output = self.sp.process_gcode(gcodes)
         self.assertEqual(expected_output, got_output)
 
@@ -57,6 +75,26 @@ class Skeinforge50ProcessorTests(unittest.TestCase):
         got_output = self.sp.process_gcode(gcodes)
         self.assertEqual(expected_output, got_output)
 
+
+class TestSkeinforgeVersioner(unittest.TestCase):
+
+    def setUp(self):
+        self.version = 50
+        self.vp = makerbot_driver.GcodeProcessors.SkeinforgeVersionChecker(
+            self.version)
+
+    def tearDown(self):
+        self.vp = None
+
+    def test_version_check_good_version(self):
+        line = "(*  using Skeinforge (50)  *)"
+        output = self.vp._transform_code(line)
+        self.assertEqual([line], output)
+
+    def test_version_check_bad_version(self):
+        line = "(*  using Skeinforge (49)  *)"
+        self.assertRaises(makerbot_driver.GcodeProcessors.VersionError,
+                          self.vp._transform_code, line)
 
 if __name__ == '__main__':
     unittest.main()
