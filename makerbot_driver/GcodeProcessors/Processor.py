5ac6cde9a80e82432bf1ad805f9773e29190e1e3
Far McKon <farmckon@gmail.com>
2012-10-11 18:14:01 -0400

    minor pep layout cleanup

--- makerbot_driver/GcodeProcessors/Processor.py
+++ makerbot_driver/GcodeProcessors/Processor.py
@@ -7,15 +7,18 @@ import threading
 
 import makerbot_driver
 
+
 class Processor(object):
     """ Base class for all Gcode Processors."""
     def __init__(self):
         self._external_stop = False
+        # ^ set this to true from another thread to stop a processor
         self._condition = threading.Condition()
+        # ^ used for all of Processor internal locking
         self.is_bundleable = False
-    
+
     def process_gcode(self, gcodes, percentCallback=None):
-        """ Abstract method to call gcode processing. Child funcctions
+        """ Abstract method to call gcode processing. Child functions
         MUST call 'testForExternalStop() before they exit
         @param callback is expected to be a callback that takes a int value
         0 to 100 of percent of processing complete
@@ -24,13 +27,19 @@ class Processor(object):
         self.test_for_external_stop()
         raise NotImplementedError("Unmplemented abstract method")
 
-    @classmethod 
-    def remove_variables(cls, gcode):
-        """removes the specified gcode variables, which are specified as #X """
+    @classmethod
+    def remove_variables(cls, gcode, newvalue='0'):
+        """
+        removes the specified gcode variables with '0'
+        gcode variables are specified as #VALUE
+        @param gcode list or iterable of gcode values
+        @param newvalue: replacement value, '0' if undefined
+        @return a new gcode, with all variable replaced with newvalue
+        """
         variable_regex = "#[^ ^\n^\r]*"
         m = re.search(variable_regex, gcode)
         while m is not None:
-            gcode = gcode.replace(m.group(), '0')
+            gcode = gcode.replace(m.group(), newvalue)
             m = re.search(variable_regex, gcode)
         return gcode
 
@@ -41,19 +50,19 @@ class Processor(object):
         return percent
 
     def set_external_stop(self, value=True):
-        """ set this if you want to interrupt this processor during it's next 
-        loop through process_gcode 
+        """ set True when you want to  interrupt a processor
+        during the next iteration of process_gcode to throw
+        an ExternalStopError. Used to cancel/break processing
         """
         with self._condition:
-            self._external_stop = value 
+            self._external_stop = value
 
     def test_for_external_stop(self):
-        """ If an external stop is set, this function will throw an 
-        ExternalStopError. This is used so a processing thread can be 
-        interrupted from another context if needed. Inherited implementions 
+        """ If an external stop is set, this function will throw an
+        ExternalStopError. This is used so a processing thread can be
+        interrupted from another context if needed. Inherited implementions
         of process_gcode MUST call this function.
         """
         with self._condition:
             if self._external_stop:
                 raise makerbot_driver.ExternalStopError
- 
