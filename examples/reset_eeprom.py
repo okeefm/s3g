9cd8911da46feee227f0c5c1dc7f57dc692289b2
wdcrith <wdc.rith@gmail.com>
2013-01-15 13:24:44 -0500

    Updated reset_eeprom.py to auto detect connected bot, and reset its EEPROM fully, without commandline args

--- examples/reset_eeprom.py
+++ examples/reset_eeprom.py
@@ -15,22 +15,23 @@ parser.add_option("-p", "--port", dest="port",
 parser.add_option("-m", "--machine_type", dest="machine",
                   help="machine type", default="The Replicator")
 parser.add_option("-v", "--version", dest="version",
-                  help="version you want to upload to", default="5.5")
+                 help="version you want to upload to", default="5.5")
 (options, args) = parser.parse_args()
 
 if options.port is None:
     md = makerbot_driver.MachineDetector()
-    md.scan(options.machine)
+    md.scan()
     port = md.get_first_machine()
     if port is None:
-        print "Cant Find %s" % (options.machine)
+        print "Cant Find %s" % ("A Printer")
         sys.exit()
 else:
     port = options.port
-factory = makerbot_driver.MachineFactory()
-returnobj = factory.build_from_port(port)
-r = getattr(returnobj, 's3g')
 
+print '\nPORT: ' + port
+print "Fully reseting EEPROM, this will take a couple seconds"
+print "------------------------------------------------------"
+r = makerbot_driver.s3g.from_filename(port)
 eeprom_length = 4000
 value = struct.pack('<B', 0xFF)
 for i in range(eeprom_length):
