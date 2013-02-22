e720b9ecfe652f31a02a012651f9ec102f514326
dgs3 <sayles.dave@gmail.com>
2013-02-22 17:46:50 -0500

    Added imports for Rep2XPurge processors, bugfil for Rep2XSinglePurgeProcessor

--- makerbot_driver/GcodeProcessors/Rep2XSinglePurgeProcessor.py
+++ makerbot_driver/GcodeProcessors/Rep2XSinglePurgeProcessor.py
@@ -8,8 +8,11 @@ class Rep2XSinglePurgeProcessor(AnchorProcessor):
     def __init__(self):
         super(Rep2XSinglePurgeProcessor, self).__init__()
         self.looking_for_first_move = True
+        self.do_anchor = True
 
     def _transform_anchor(self, match):
+        if "(Anchor Start)" in  match.string:
+            self.do_anchor = False
         if self.looking_for_first_move:
             codes, flags, comments = makerbot_driver.Gcode.parse_line(match.string)
             purge_codes = [
@@ -18,7 +21,8 @@ class Rep2XSinglePurgeProcessor(AnchorProcessor):
                     "G1 X-105.400 Y-73.500 Z0.270 F1800.000 (Return)\n",
                     "G92 A0 B0 (Reset after purge)",
             ]
-            purge_codes.extend(super(Rep2XSinglePurgeProcessor, self)._transform_anchor(match))
+            if self.do_anchor:
+                purge_codes.extend(super(Rep2XSinglePurgeProcessor, self)._transform_anchor(match))
             self.looking_for_first_move = False
             return purge_codes
         else:
