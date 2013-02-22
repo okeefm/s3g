abc6c2bb2d0f5d9f61d949fcfca950d87b19b12b
dgs3 <sayles.dave@gmail.com>
2013-02-22 18:49:06 -0500

    Added new line

--- makerbot_driver/GcodeProcessors/Rep2XSinglePurgeProcessor.py
+++ makerbot_driver/GcodeProcessors/Rep2XSinglePurgeProcessor.py
@@ -19,7 +19,7 @@ class Rep2XSinglePurgeProcessor(AnchorProcessor):
                     "G1 X-105.400 Y-74.000 Z0.270 F1800.000 (Extruder Purge Start)\n",
                     "G1 X105.400 Y-74.000 Z0.270 F1800.000 E25.000 (Extruder Purge)\n",
                     "G1 X-105.400 Y-73.500 Z0.270 F1800.000 (Return)\n",
-                    "G92 A0 B0 (Reset after purge)",
+                    "G92 A0 B0 (Reset after purge)\n",
             ]
             if self.do_anchor:
                 purge_codes.extend(super(Rep2XSinglePurgeProcessor, self)._transform_anchor(match))
