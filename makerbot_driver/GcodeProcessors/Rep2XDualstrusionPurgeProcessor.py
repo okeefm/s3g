147a0205a4ddee7db29309f08cab62c6af7957a8
dgs3 <sayles.dave@gmail.com>
2013-02-23 13:50:31 -0500

    Updated Dual Purge Processor to handle the first inactive retract

--- makerbot_driver/GcodeProcessors/Rep2XDualstrusionPurgeProcessor.py
+++ makerbot_driver/GcodeProcessors/Rep2XDualstrusionPurgeProcessor.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import re
+import makerbot_driver
 
 from .LineTransformProcessor import LineTransformProcessor
 
@@ -20,17 +21,34 @@ class Rep2XDualstrusionPurgeProcessor(LineTransformProcessor):
         self.current_toolchange = match.string
         return self.current_toolchange
 
+    @staticmethod
+    def _get_inactive_toolhead(toolchange):
+        (codes, flags, comments) = makerbot_driver.Gcode.parse_line(toolchange)
+        active_toolhead = codes['T']
+        inactive_code_map = {0: 'B', 1: 'A'}
+        return inactive_code_map[active_toolhead]
+
+    def _get_retract_commands(self, profile, toolchange):
+        inactive_toolhead = self._get_inactive_toolhead(toolchange)
+        retract_commands = [
+            "M135 T%i\n" % (0 if inactive_toolhead == 'A' else 1),
+            "G1 %s-%i F%i\n" % (inactive_toolhead, profile.values['dualstrusion']['retract_distance_mm'], profile.values['dualstrusion']['snort_feedrate']),
+            "G92 %s0\n" % (inactive_toolhead),
+        ]
+        return retract_commands
+
     def _add_purge(self, match):
         toadd = []
         if self.looking_for_first_move:
-                toadd.extend([
-                        "M135 T0\n",
-                        "G1 X-105.400 Y-74.000 Z0.270 F1800.000 (Right Purge Start)\n",
-                        "G1 X105.400 Y-74.000 Z0.270 F1800.000 A25.000 (Right Purge)\n",
-                        "M135 T1\n",
-                        "G1 X105.400 Y-73.500 Z0.270 F1800.000 (Left Purge Start)\n",
-                        "G1 X-105.400 Y-73.500 Z0.270 F1800.000 B25.000 (Left Purge)\n",
-                        "G92 A0 B0 (Reset after purge)\n",
-                ])
+            toadd.extend([
+                "M135 T0\n",
+                "G1 X-105.400 Y-74.000 Z0.270 F1800.000 (Right Purge Start)\n",
+                "G1 X105.400 Y-74.000 Z0.270 F1800.000 A25.000 (Right Purge)\n",
+                "M135 T1\n",
+                "G1 X105.400 Y-73.500 Z0.270 F1800.000 (Left Purge Start)\n",
+                "G1 X-105.400 Y-73.500 Z0.270 F1800.000 B25.000 (Left Purge)\n",
+                "G92 A0 B0 (Reset after purge)\n",
+            ])
+            toadd.extend(self._get_retract_commands(self.profile, self.current_toolchange))
         self.looking_for_first_move = False
         return toadd + [self.current_toolchange, match.string]
