9d3df8fc22616870d60248a034829d87f3e7e62c
wdcrith <wdc.rith@gmail.com>
2013-04-09 11:48:56 -0400

    Untested: Iterator based ProgressProcessor

--- makerbot_driver/GcodeProcessors/ProgressProcessor.py
+++ makerbot_driver/GcodeProcessors/ProgressProcessor.py
@@ -17,24 +17,24 @@ class ProgressProcessor(Processor):
         progressmsg = "M73 P%s (progress (%s%%))\n" % (percent, percent)
         return progressmsg
 
-    def process_gcode(self, gcodes, callback=None):
-        output = []
-        count_total = len(gcodes)
-        count_current = 0
+    def process_gcode(self, gcodes, gcode_info, callback=None):
+
+        total_bytes = gcode_info['size_in_bytes']
+
+        current_bytes = 0
         current_percent = 0
         for code in gcodes:
-            count_current += 1
-            output.append(code)
-            new_percent = int(100.0 * count_current / count_total)
+            current_bytes += len(code)
+            yield code
+            new_percent = int(100.0 * (current_bytes / total_bytes))
             if new_percent > current_percent:
                 progressmsg = self.create_progress_msg(new_percent)
                 with self._condition:
                     self.test_for_external_stop(prelocked=True)
-                    output.append(progressmsg)
+                    yield progressmsg
                 current_percent = new_percent
                 if callback is not None:
                     callback(current_percent)
-        return output
 
 
 def main():
